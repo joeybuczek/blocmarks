1bde77db4b4f23764479716bd525d0fb6a883f2b
Joey Buczek <joe.buczek@gmail.com>
2015-04-28 02:35:50 +0000

    added views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
+  root to: 'welcome#index'
+  
+  get 'about' => 'welcome#about'
+  resources :topics
+  post :incoming, to: "incoming#create"
   
   devise_for :users
-  root to: 'welcome#index'
-  get 'about' => 'welcome#about'
-  post :incoming, to: "incoming#create"
 
 end
