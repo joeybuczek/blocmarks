3874a20385efb6c053d7d583e7bca097776b9ab9
Joey Buczek <joe.buczek@gmail.com>
2015-04-28 02:04:30 +0000

    mailgun testing 5

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -5,10 +5,25 @@ class IncomingController < ApplicationController
   def create
     
     @user = User.find_by_email(params[:sender])
+    @topic = @user.topics.find_by_title(params[:subject])
 
-    @user.name = "Received"
-    @user.save
+    if @user.nil?
+      @user = User.new(
+        name: params[:from],
+        email: params[:sender],
+        password: "12345678" # assign initial password for now...
+      )
+      @user.skip_confirmation! # skip for now...
+      @user.save
+    end
     
+    if @topic.nil?
+      @topic = @user.topics.build(title: params[:subject])
+      @topic.save
+    end
+    
+    @bookmark = @topic.bookmarks.build(url: params["body-plain"])
+    @bookmark.save
   end
   
 end
