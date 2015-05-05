1f0449aafc481d4c9a140f26b610463e0fd0b28b
Joey Buczek <joe.buczek@gmail.com>
2015-05-05 05:04:12 +0000

    added Like model and liked() method to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :topics, dependent: :destroy
   has_many :bookmarks
+  has_many :likes, dependent: :destroy
   
 #   after_initialize :set_defaults
 
   def set_defaults
     self.name = "Testing"
+  end
+  
+  def liked(bookmark)
+    likes.where(bookmark_id: bookmark.id).first
   end
   
 end
