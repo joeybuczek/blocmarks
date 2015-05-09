6654f5583cbeec1896509a0288ea2f7540a21dc7
Joey Buczek <joe.buczek@gmail.com>
2015-05-09 01:22:12 +0000

    added user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def show
+    @user_bookmarks = current_user.bookmarks.all
+    @liked_bookmarks = Bookmark.where(id: current_user.likes.pluck(:bookmark_id))
   end
 end
