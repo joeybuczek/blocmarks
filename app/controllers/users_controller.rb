b524c6117d7d6cc898e3cf5e46faad522ed38b52
Joey Buczek <joe.buczek@gmail.com>
2015-05-10 02:43:51 +0000

    testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user_bookmarks = current_user.bookmarks.all
     @liked_bookmarks = Bookmark.where(id: current_user.likes.pluck(:bookmark_id))
+    # @liked_bookmarks = current_user.liked_bookmarks.all
   end
 end
